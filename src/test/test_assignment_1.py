54555cca89348d14c2ea5e31af6e352033f2ae12
Leah Tomberg <166473077+leaht13@users.noreply.github.com>
2025-01-31 23:34:52 -0500

    Update test_assignment_1.py

--- src/test/test_assignment_1.py
+++ src/test/test_assignment_1.py
@@ -1,1 +1,24 @@
 
+# Approximation Algorithm
+x0 = 1.5
+tol = 0.000001
+
+iter = 0
+diff = x0
+x = x0
+
+print(f"{iter} : {x}")
+
+while diff >= tol:
+    iter += 1
+    y = x
+    
+    x = (x / 2) + (1 / x)
+    
+    print(f"{iter} : {x}")
+    
+    diff = abs(x - y)
+
+print(f"\nConvergence after {iter} iterations")
+
+
